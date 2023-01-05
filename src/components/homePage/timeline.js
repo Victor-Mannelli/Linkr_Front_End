ba5b47d906f387d810f25f79964afa8b7d9d345d
nandasino <fernandamsino@gmail.com>
2023-01-05 17:21:23 -0300

    formulário de publicação pronto e aviso de loading ou sem publicações na timeline

---
+++ src/components/homePage/timeline.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import Publicacao from "./publicacao";
+import Posts from "./posts";
+
+export default function Timeline() {
+    return(
+        <Container>
+        <div className="title">timeline</div>
+        <Publicacao/>
+        <Posts/>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 150px;
+    width: 611px;
+
+    .title{
+        width:100%;
+        margin-bottom:40px;
+        color: #FFFFFF;
+        font-family: 'Oswald';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 43px;
+        line-height: 64px;
+    }
+
+    @media(max-width: 614px){
+        width: 100%;
+
+        .title{
+            margin-bottom:20px;
+            padding-left:15px
+        }
+    
+    }
+`
---
+++ src/service/server.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL ="http://localhost:5000";
+
+
+export function postUrl(post,token) {
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+    const promise = axios.post(`${BASE_URL}/post`, post, config);
+    return promise;
+}
+
+export function getPosts(token){
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+    const promise = axios.get(`${BASE_URL}/post`, config);
+    return promise;
+}
