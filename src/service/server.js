5e9b6f881c89ac30de56fe03cac01cbbf2a1634a
Victor-Mannelli <imp.mannelli@gmail.com>
2023-01-11 16:05:28 -0300

    feat: add user page and function to travel to other user pages through their profile picture

--- src/service/server.js
+++ src/service/server.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export function postUrl(post, config) {
-    const promise = axios.post(`${process.env.REACT_APP_API}/post`, post, config);
-    return promise;
-}
-
 export function getPosts(config) {
     const promise = axios.get(`${process.env.REACT_APP_API}/post`, config);
     return promise;
